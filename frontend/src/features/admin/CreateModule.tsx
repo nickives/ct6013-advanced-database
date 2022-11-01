40e34313b258bcc1ec98a35e8462c8ddf086902f
Nick Ives <nickives@gmail.com>
2022-11-01 20:35:26 +0000

    Frontend: Admin index

---
+++ frontend/src/features/admin/CreateModule.tsx
@@ -0,0 +1,10 @@
+import { Container } from '@mui/material';
+import React from 'react';
+
+const CreateModule = () => (
+  <Container>
+    Create Module
+  </Container>
+);
+
+export default CreateModule;
---
+++ frontend/src/features/admin/index.ts
@@ -0,0 +1,3 @@
+export { default as CreateCourse } from './CreateCourse';
+export { default as CreateModule } from './CreateModule';
+export { default as CreateLecturer } from './CreateLecturer';
