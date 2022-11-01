b40bb6a751a41264e1c31e0cff1f1ad433d7fdfa
Nick Ives <nickives@gmail.com>
2022-11-01 20:33:05 +0000

    Frontend: Initial routing

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// LIBRARIES
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from 'react-router-dom';
+
+// COMPONENTS
+import { CreateCourse, CreateModule, CreateLecturer } from 'features/admin';
+import { NavbarProvider } from 'features/navbar';
+import { AdminRoot, AdminIndex, ErrorPage, Home, Root, NoMatch } from 'routes';
+
+// LOCAL
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+if (process.env.NODE_ENV === 'development') {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires, global-require
+  const { worker } = require('./mocks/browser');
+  worker.start();
+}
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={ <Root /> } errorElement={ <ErrorPage /> }>
+      <Route index element={ <Home /> } />
+      <Route path="/admin" element={ <AdminRoot /> }>
+        <Route index element={ <AdminIndex /> } />
+        <Route path="create-course" element={ <CreateCourse /> } />
+        <Route path="create-module" element={ <CreateModule /> } />
+        <Route path="create-lecturer" element={ <CreateLecturer /> } />
+      </Route>
+      <Route path="*" element={ <NoMatch /> } />
+    </Route>,
+  ),
+);
+
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <NavbarProvider>
+      <RouterProvider router={ router } />
+    </NavbarProvider>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
