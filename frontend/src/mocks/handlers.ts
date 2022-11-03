79b79e9f94961acc5e11a124a5863d0d045f9ce1
Nick Ives <nickives@gmail.com>
2022-11-03 14:46:23 +0000

    Frontend: Course & module mocks

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -1,13 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { rest } from 'msw';
+import { Course, Lecturer, Module } from 'types';
 
 // MOCK DATA
 // LECTURERS
 const lecturerName = 'John Doe ';
-const lecturers: {
-  id: string;
-  name: string;
-}[] = [];
+const lecturers: Lecturer[] = [];
 for (let i = 0; i < 100; i += 1) {
   lecturers.push({
     id: i.toString(),
@@ -16,8 +14,26 @@ for (let i = 0; i < 100; i += 1) {
 }
 
 // COURSES
+const courseName = 'Course ';
+const courses: Course[] = [];
+for (let i = 0; i < 100; i += 1) {
+  courses.push({
+    id: i.toString(),
+    name: courseName + i,
+  });
+}
 
 // MODULES
+const moduleName = 'Module ';
+const modules: Module[] = [];
+for (let i = 0; i < 100; i += 1) {
+  modules.push({
+    id: i.toString(),
+    name: moduleName + i,
+    courseId: (i + 1).toString(),
+    lecturerId: (i + 2).toString(),
+  });
+}
 
 // eslint-disable-next-line import/prefer-default-export
 export const handlers = [
@@ -45,7 +61,7 @@ export const handlers = [
   // Handles a POST lecturers request
   rest.post('/api/lecturers', async (req, res, ctx) => { // 2
     const { page, limit }: { page: number; limit: number } = await req.json();
-    const start = (page - 1) * limit;
+    const start = page * limit;
     const end = start + limit;
     const data = lecturers.slice(start, end);
     return res(
@@ -71,6 +87,50 @@ export const handlers = [
         id: '1',
         name: name,
       }),
+    );
+  }),
+
+  // Handles a POST create-module request
+  rest.post('/api/create-module', async (req, res, ctx) => { // 4
+    const {
+      name, courseId, lecturerId,
+    }: {
+      name: string; courseId: string; lecturerId: string;
+    } = await req.json();
+    if (name === 'errorTest') {
+      return res(
+        ctx.status(500),
+        ctx.json({
+          error: 'Internal Server Error',
+        }),
+      );
+    }
+    if (name === 'uniqueErrorTest') {
+      return res(
+        ctx.status(409),
+        ctx.json({
+          error: 'Error: Module name already exists',
+        }),
+      );
+    }
+    return res(
+      ctx.status(201),
+      ctx.json({
+        id: '1',
+        name: name,
+      }),
+    );
+  }),
+
+  // Handles a POST courses request
+  rest.post('/api/courses', async (req, res, ctx) => { // 2
+    const { page, limit }: { page: number; limit: number } = await req.json();
+    const start = page * limit;
+    const end = start + limit;
+    const data = courses.slice(start, end);
+    return res(
+      ctx.status(200),
+      ctx.json(data),
     );
   }),
 ];
